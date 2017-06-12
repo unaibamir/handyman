981e81e46195d0e9217781428439fb4a02e69459
unaibamir <unaibamiraziz@gmail.com>
2017-06-12 17:11:06 +0500

    added custom js file and removed unnecessary scripts

---
+++ public/js/functions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Unaib on 12/6/2017.
+ */
+
+$(function () {
+    $(".select2").select2();
+});
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,25 +4,17 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
     <title>@yield('page_title') | {{ config('app.name') }} | Dashboard</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    @yield('meta')
 
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
-
-    <!-- Toastr style -->
-    <link href="{{ asset('css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
-
-    <!-- Gritter -->
-    <link href="{{ asset('js/plugins/gritter/jquery.gritter.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/plugins/select2/select2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/plugins/footable/footable.core.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
 
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @yield('stylesheets')
 
 </head>
@@ -49,34 +41,17 @@
     <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
     <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
-    <!-- Flot -->
-    <script src="{{ asset('js/plugins/flot/jquery.flot.js') }}"></script>
-    <script src="{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
-    <script src="{{ asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>
-    <script src="{{ asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>
-    <script src="{{ asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>
-
-    <!-- Peity -->
-    <script src="{{ asset('js/plugins/peity/jquery.peity.min.js') }}"></script>
-    <script src="{{ asset('js/demo/peity-demo.js') }}"></script>
-
     <!-- Custom and plugin javascript -->
     <script src="{{ asset('js/inspinia.js') }}"></script>
     <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
 
     <!-- jQuery UI -->
     <script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-
-
-    <!-- ChartJS-->
-    <script src="{{ asset('js/plugins/chartJs/Chart.min.js') }}"></script>
-
-    <!-- Toastr -->
-    <script src="{{ asset('js/plugins/toastr/toastr.min.js') }}"></script>
-
     <!-- FooTable -->
     <script src="{{ asset('js/plugins/footable/footable.all.min.js') }}"></script>
 
+    <script src="{{ asset('js/plugins/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('js/functions.js') }}"></script>
     @yield('scripts')
 </body>
 </html>
