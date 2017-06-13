c74fbefdab02335f4dbda2a373f4c9e7005c1785
unaibamir <unaibamiraziz@gmail.com>
2017-06-13 14:30:14 +0500

    changed title attribute value

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>@yield('page_title') | {{ config('app.name') }} | Dashboard</title>
+    <title>@yield('page_title') | {{ config('app.name') }} Dashboard</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     @yield('meta')
 
